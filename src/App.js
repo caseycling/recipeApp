0de7286d5afd6262d6ee827613591e7df5455aeb
Casey <caseyclinga@gmail.com>
2020-01-05 10:48:16 -0800

    Initialized state and useEffect function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
-  const APP_ID = 'b2630a22';
-  const APP_KEY = 'bf41122161d5eef385038956b189eb18';	
-
+  const APP_ID = "b2630a22";
+  const APP_KEY = "bf41122161d5eef385038956b189eb18";	
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, []);
 
   return(
     <div className="App">
       <form className="search-form">
         <input classname="search-bar" type="text"/>
-          <button classnam="search-button" type="submit">
-            Search
-          </button>
+          <button classname="search-button" type="submit">Search</button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 };
