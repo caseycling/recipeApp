6c5245299462ba3e6809302f86b49b6f3e3e77fd
Casey <caseyclinga@gmail.com>
2020-01-05 11:15:23 -0800

    Created state for recipes response to be stored in setRecipes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const App = () => {
   const APP_ID = "b2630a22";
   const APP_KEY = "bf41122161d5eef385038956b189eb18";	
 
+  const [recipes, setRecipes] = useState([]);
+
 
   useEffect(() => {
     getRecipes()
@@ -14,7 +16,8 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(setRecipes)
   }
 
   return(
