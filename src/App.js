7026eebdd59bf3bd1f7a65a7abcf52e9242f4b55
Casey <caseyclinga@gmail.com>
2020-01-05 19:55:19 -0800

    Added placeholder text

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input className="search-bar" type="text" placeholder="Enter an ingredient to search for related recipes" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
       <div className="recipes">
