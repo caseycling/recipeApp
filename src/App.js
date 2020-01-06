04c4965d7a46a2f1a01084dcfca974131738013d
Casey <caseyclinga@gmail.com>
2020-01-05 19:05:34 -0800

    Added function to allow for change in input value

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
   const APP_KEY = "bf41122161d5eef385038956b189eb18";	
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  
 
 
   useEffect(() => {
@@ -18,19 +20,26 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits)
+    console.log(data);
+    console.log(recipes);
+  };
+
+  const updateSearch = event => {
+    setSearch(event.target.value);
+    console.log(search);
   }
 
   return(
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"/>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
           <button className="search-button" type="submit">Search</button>
       </form>
       {recipes.map(recipe => (
         <Recipe 
+          key={recipe.recipe.label}
           title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
+          calories={Math.floor(recipe.recipe.calories)} 
           image={recipe.recipe.image}
         />
       ))}
