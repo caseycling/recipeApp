16b15f73597ce228da9594b54674fadb75be1f3f
Casey <caseyclinga@gmail.com>
2020-01-05 19:39:37 -0800

    Added div container

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ const App = () => {
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
           <button className="search-button" type="submit">Search</button>
       </form>
+      <div className="recipes">
       {recipes.map(recipe => (
         <Recipe 
           key={recipe.recipe.label}
@@ -50,6 +51,7 @@ const App = () => {
           ingredients={recipe.recipe.ingredients}
         />
       ))}
+      </div>
     </div>
   );
 };
