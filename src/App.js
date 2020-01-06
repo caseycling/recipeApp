65da918077e5b6c3786c7e393fe32377ab5650bc
Casey <caseyclinga@gmail.com>
2020-01-05 18:32:39 -0800

    Rendered recipe component

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(setRecipes)
+    console.log(data.hits)
   }
 
   return(
@@ -27,6 +27,13 @@ const App = () => {
         <input className="search-bar" type="text"/>
           <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
