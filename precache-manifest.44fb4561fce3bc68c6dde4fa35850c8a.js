46bdbe082a2f17aeeed5e0451096fc009d4a0fd8
Casey <caseyclinga@gmail.com>
2020-01-28 18:26:18 -0800

    Updates

---
+++ precache-manifest.44fb4561fce3bc68c6dde4fa35850c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd136b2f0ecf3b69de1427c4e6be69c6",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "cef8be93c5d99dc242b6",
+    "url": "/recipeApp/static/css/main.990ce635.chunk.css"
+  },
+  {
+    "revision": "6e29f9b076f3b0b7bf2f",
+    "url": "/recipeApp/static/js/2.fa1dfd89.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipeApp/static/js/2.fa1dfd89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cef8be93c5d99dc242b6",
+    "url": "/recipeApp/static/js/main.a2bfb39e.chunk.js"
+  },
+  {
+    "revision": "11b883d7025fd222c023",
+    "url": "/recipeApp/static/js/runtime-main.221b97f2.js"
+  },
+  {
+    "revision": "45ef0571d1fc831fdb4c2d917157c508",
+    "url": "/recipeApp/static/media/Recipe_logo.45ef0571.jpeg"
+  }
+]);
